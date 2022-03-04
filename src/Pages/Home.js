9ab4608c480fd5c0fbd64104c7d6e25bf1206c59
Cody Gant <codygantol@gmail.com>
2022-03-03 23:43:24 -0500

    first commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "../components/Header";
+
 import Broll from "../assests/Broll.mp4";
-import building from "../assests/download.jpg";
-import { Button, Container, Col, Row } from "react-bootstrap";
+
+import { Button, Container } from "react-bootstrap";
 
 const Home = () => {
   return (
