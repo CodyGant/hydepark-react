940c93fe004828ab8649e741843c7a5aa388f1b1
cody gant <codygantol@gmail.com>
2022-03-21 13:50:10 -0400

    Locations page

--- src/Pages/Locations.js
+++ src/Pages/Locations.js
@@ -1,14 +1,203 @@
 import React from "react";
 import Header from "../components/Header";
-import { Button, Container } from "react-bootstrap";
+import { Card, Container, CardGroup, Button } from "react-bootstrap";
 const Locations = () => {
   return (
     <div>
       <Header />
-      <Container>
-        <Button className="submitButton" variant="success" type="submit">
-          Submit
-        </Button>
+      <Container fluid className="Locations">
+        <div className = 'locationscontain'>
+        <h1>Now eleven Restarunts throughout the United States!</h1>
+        <CardGroup>
+                <Card data-aos="flip-left" className="locations-card">
+                 
+                  <Card.Body>
+                    <Card.Title>Sarasota, FL</Card.Title>
+                    <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                    <br />
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                    
+                    
+                    
+                  </Card.Body>
+                  <Button  className ='locations-bottom-button' href = '/privateDining'>View our menu!</Button>
+                </Card>
+
+                <Card data-aos="flip-right" className = 'locations-card'>
+                 
+                  <Card.Body >
+                    <Card.Title>Dublin, OH</Card.Title>
+                    <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                    <br />
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                    
+                  </Card.Body>
+                  <Button  className ='locations-bottom-button' href = '/privateDining'>View our menu!</Button>
+                </Card>
+                </CardGroup>
+                <CardGroup>
+                <Card data-aos="flip-right" className="locations-card">
+                  
+                  <Card.Body>
+                    <Card.Title>Birmingham, MI</Card.Title>
+                    <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                    <br />
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                    
+                  </Card.Body>
+                  <Button  className ='locations-bottom-button' href = '/earlynights'>View our menu!</Button>
+                </Card>
+
+                <Card data-aos="flip-left" className="locations-card">
+                  
+                  <Card.Body>
+                    <Card.Title>Beachwood, OH</Card.Title>
+                    <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                    <br />
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                    
+                  </Card.Body>
+                  <Button  className ='locations-bottom-button' href = '/privateDining'>View our menu!</Button>
+                </Card>
+                </CardGroup>
+                <CardGroup>
+                <Card data-aos="flip-right" className="locations-card">
+                  
+                  <Card.Body>
+                    <Card.Title>Upper Arlington, OH</Card.Title>
+                    <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                    <br />
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                    <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                    
+                  </Card.Body>
+                  <Button  className ='locations-bottom-button' href = '/giftcards'>View our menu!</Button>
+                </Card>
+             
+              <Card data-aos="flip-left" className="locations-card">
+                 
+                 <Card.Body>
+                   <Card.Title>Downtown Columbus, OH</Card.Title>
+                   <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                   <br/>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                   
+                   
+                   
+                 </Card.Body>
+                 <Button  className ='locations-bottom-button' href = '/privateDining'>View our menu!</Button>
+               </Card>
+</CardGroup>
+               <CardGroup>
+               <Card data-aos="flip-right" className = 'locations-card'>
+                
+                 <Card.Body>
+                   <Card.Title>Northville, MI</Card.Title>
+                   <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                   <br />
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                   
+                 </Card.Body>
+                 <Button  className ='locations-bottom-button' href = '/privateDining'>View our menu!</Button>
+               </Card>
+               
+               <Card data-aos="flip-right" className="locations-card">
+                 
+                 <Card.Body>
+                   <Card.Title>Daytona Beach, FL</Card.Title>
+                   <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                   <br />
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                   
+                 </Card.Body>
+                 <Button  className ='locations-bottom-button' href = '/earlynights'>View our menu!</Button>
+               </Card>
+               </CardGroup>
+               <CardGroup>
+               <Card data-aos="flip-left" className="locations-card">
+                 
+                 <Card.Body>
+                   <Card.Title>Westlake, OH</Card.Title>
+                   <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                   <br />
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                   
+                 </Card.Body>
+                 <Button  className ='locations-bottom-button' href = '/privateDining'>View our menu!</Button>
+               </Card>
+             
+               <Card data-aos="flip-right" className="locations-card">
+                 
+                 <Card.Body>
+                   <Card.Title>Pittsburgh, PA</Card.Title>
+                   <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                   <br />
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                   
+                 </Card.Body>
+                 <Button  className ='locations-bottom-button' href = '/giftcards'>View our menu!</Button>
+               </Card>
+               </CardGroup>
+               <CardGroup>
+              <Card data-aos="flip-left" className="locations-card" id = 'bottom-location'>
+                 
+                 <Card.Body>
+                   <Card.Title>Indianapolis, IN</Card.Title>
+                   <Card.Text>Dine-in • Carryout • Curbside • Delivery</Card.Text>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Open Table</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Take Out</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>Uber Eats</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>GrubHub</Button>
+                   <Button  className ='LocationsBtn' href = '/privateDining'>DoorDash</Button>
+                   
+                   
+                   
+                 </Card.Body>
+                 <Button  className ='locations-bottom-button' href = '/privateDining'>View our menu!</Button>
+               </Card>
+
+               
+              </CardGroup>
+              </div>
       </Container>
     </div>
   );
